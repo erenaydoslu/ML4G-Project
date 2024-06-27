402f36c7a5d186e60d89cb38caace3d2fc7e6db6
Cem Levi <levicem@hotmail.com>
2024-06-27 15:29:45 +0200

    switched to using full product matrix

--- product_graph.py
+++ product_graph.py
@@ -1,7 +1,10 @@
 import numpy as np
 import networkx as nx
-def generate_parametric_product_graph(s00, s01, s10, s11, A_T, A_N, spatial_graph):
+import scipy as sp
+from scipy.sparse import csr_array
+def generate_parametric_product_graph(s00, s01, s10, s11, A_T, A_N, spatial_graph = None):
     #print("Generating product graph...")
+    '''
     I_T = np.identity(len(A_T))
     I_N = np.identity(len(A_N))
     S_diamond = (
@@ -11,6 +14,18 @@ def generate_parametric_product_graph(s00, s01, s10, s11, A_T, A_N, spatial_grap
         s11 * np.kron(A_T, A_N)
     )
     product_graph = nx.from_numpy_array(S_diamond)
+    '''
+    I_T = csr_array(np.identity(len(A_T)))
+    I_N = csr_array(np.identity(len(A_N)))
+    A_T = csr_array(A_T)
+    A_N = csr_array(A_N)
+    S_diamond = (
+        s00 * sp.sparse.kron(I_T, I_N) +
+        s01 * sp.sparse.kron(I_T, A_N) +
+        s10 * sp.sparse.kron(A_T, I_N) +
+        s11 * sp.sparse.kron(A_T, A_N)
+    )
+    return S_diamond
     '''
     
     # Add features to the product graph
